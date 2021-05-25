ea08fb8da74c00ad2b94b02cc9ec01761b896735
pjd6589 <pjd6587@naver.com>
2021-05-25 15:11:35 +0900

    로그인 구현

--- models/member.js
+++ models/member.js
@@ -15,16 +15,50 @@ const member = {
 	* @return Boolean
 	*/
 	join : async function (memId, memPw) {
+		try{
 		const hash = await bcrypt.hash(memPw, 10);
 		
 		const sql = "INSERT INTO member (memId, memPw) VALUES (:memId, :memPw)";
-		
-		const result = await sequelize.query(sql, {
+		await sequelize.query(sql, {
 			replacements : { memId, memPw : hash },
 			type : QueryTypes.INSERT,
 		});
-		
+			
+			return true;
+		} catch (err) {
 		console.log(result);
+		}
+		
+	},
+	/**
+	* 로그인
+	*
+	* @param String memId 아이디
+	* @param String memPw 비밀번호
+	* @param Object req - Request 객체 
+	*
+	* @return Boolean
+	*/
+	login : async function(memId, memPw) {
+		const sql = "SELECT * FROM member WHERE memId = ? AND isAdmin=1";
+		const rows = await sequelize.query(sql, {
+			replacements : [memId],
+			type : QueryTypes.SELECT,
+		});
+		
+		if (rows.length == 0) { // 회원이 존재 X
+			return false;
+		}
+		
+		// 회원이 존재하면 비밀번호 일치 여부 체크 
+		const match = await bcrypt.compare(memPw, rows[0].memPw);
+		if (match) { // 비밀번호 일치 
+			req.session.memId = rows[0].memId;
+			return true;
+		}
+		
+		// 로그인 실패
+		return false;
 	},
 };
 
