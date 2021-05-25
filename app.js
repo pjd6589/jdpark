825e06ec6a5f9f981216fc57b46ee75f6026e852
pjd6589 <pjd6587@naver.com>
2021-05-25 12:21:51 +0900

    라우터 구성, 기본템플릿 설정

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const { sequelize } = require("./models");
+
+/** 라우터 */
+const memberRouter = require('./routes/member');
 
 dotenv.config();
 
@@ -17,18 +21,34 @@ nunjucks.configure("views", {
 	watch : true,
 });
 
+/** DB 연결 */
+sequelize.sync({ force : false })
+			.then(() => {
+				console.log("데이터베이스 연결 성공!");
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+
 app.use(morgan('dev'));
 app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser(process.env.COOKE_SECRET)); // 쿠키 설정
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 쿠기 설정 
 app.use(session({
 	resave: false,
-	saveUninitalized : false,
+	saveUninitialized : false,
+	cookie : {
+		httpOnly : true,
+		secure : false,
+	},
+	name : 'jdsession',
 }));
 
 
+/** 라우터 등록 */
+app.use("/member", memberRouter);
 
 // 없는 페이지 처리 미들웨어(라우터)
 app.use((req, res, next) => {
