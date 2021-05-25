c666f0e32305c052ddc72af4681a949e0b48e12a
pjd6589 <pjd6587@naver.com>
2021-05-25 17:11:23 +0900

    관리자 페이지 기본 스타일

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	
-	return res.send("관리자 페이지");
+	const params = {
+		addClass : 'admin_page',
+	};
+	return res.send("admin/main");
 });
 
 module.exports = router;
