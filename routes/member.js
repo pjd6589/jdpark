03a9bf73278bb32068038ea0d3d18fd9298f63d5
pjd6589 <pjd6587@naver.com>
2021-05-27 10:21:40 +0900

    취업 우대, 병역 양식 작업

--- routes/member.js
+++ routes/member.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const member = require("../models/member"); // member Model 
 const { joinValidator } = require('../middlewares/join_validator'); // 회원 가입 유효성 검사
-const { alert, go } = require("../lib/common");
+const { alert, go } = require("../lib/common"); // 공통 함수 
 
-/** 회원가입 S */
+/** 회원 가입 S */
 router.route("/join")
 		/** 회원 가입 양식 */
 		.get((req, res, next) => {
@@ -13,23 +13,23 @@ router.route("/join")
 		/** 회원 가입 처리 */
 		.post(joinValidator, async (req, res, next) => {
 			try {
-			const result = await member.join(req.body.memId, req.body.memPw);
-			if (result) {
+				const result = await member.join(req.body.memId, req.body.memPw);
+				if (result) { // 성공 
 					return go("/member/login", res, "parent");
-				}
+				} 
 			} catch (err) {
 				console.error(err);
 				next(err);
 			}
 			
-			// 실패한 경우
+			// 실패한 경우 
 			return alert("관리자 등록요청 실패!", res);
-		}); 
+		});
 /** 회원 가입 E */
 
 /** 로그인 S */
 router.route("/login")
-			//로그인 양식
+			// 로그인 양식 
 			.get((req, res, next) => {
 				return res.render("member/login");
 			})
@@ -45,10 +45,10 @@ router.route("/login")
 					next(err);
 				}
 				
-				//로그인 실패
+				// 로그인 실패
 				return alert("로그인 실패!", res);
 			});
-			
+
 /** 로그인 E */
 
 /** 로그아웃 S */
